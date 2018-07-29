2c29603ad1afaeb21db01fbe448c4691ab7fab7e
sukubhattu <sukubhattusandesh8@gmail.com>
2018-07-29 19:10:36 +0545

    caraousel added

--- boards/views.py
+++ boards/views.py
@@ -22,7 +22,7 @@ class TopicListView(ListView):
     model = Topic
     context_object_name = 'topics'
     template_name = 'topics.html'
-    paginate_by = 10
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         kwargs['board'] = self.board
